1cf70078e0b551b5f4b1ddeebd2078a72e6215d8
ghungpham <pham.ghungj@gmail.com>
2019-06-06 13:04:47 -0400

    setting up

---
+++ data/migrations/20190606115948_first_cooking.js
@@ -0,0 +1,41 @@
+
+exports.up = function(knex, Promise) {
+  
+};
+
+exports.down = function(knex, Promise) {
+  
+};
+
+// CREATE TABLE dish (
+// 	id integer PRIMARY KEY AUTOINCREMENT,
+// 	name varchar
+// );
+
+// CREATE TABLE recipe (
+// 	id integer PRIMARY KEY AUTOINCREMENT,
+// 	name varchar,
+// 	dish_id integer,
+// 	description varchar
+// );
+
+// CREATE TABLE ingredients (
+// 	id integer PRIMARY KEY AUTOINCREMENT,
+// 	name varchar
+// );
+
+// CREATE TABLE recipes_ingredients (
+// 	id integer PRIMARY KEY AUTOINCREMENT,
+// 	recipe_id integer,
+// 	ingredient_id integer,
+// 	quantity float
+// );
+
+// CREATE TABLE get_cooking (
+// 	id integer PRIMARY KEY AUTOINCREMENT,
+// 	dish_id integer,
+// 	recipe_id integer,
+// 	recipes_ingredient integer,
+// 	quantity float
+// );
+
